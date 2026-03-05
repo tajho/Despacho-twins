b9bbd3dfa3f9332d6aa67b3fa81ba726539c0ba9
tajho <alex.201894.lol@gmail.com>
2026-03-04 21:08:41 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,10 @@
 const CACHE_NAME = 'twins-v66-cache';
-const ASSETS = [
-  './',
-  './index.html',
-  'https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;600;700;800&display=swap',
-  'https://unpkg.com/@phosphor-icons/web'
-];
+const ASSETS = ['./', './index.html'];
 
-// Instalación
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
-// Estrategia: Primero Red, si falla, Caché
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
