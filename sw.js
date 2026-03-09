80e3695d2a857716fd52a28ee5bec3bcd98f578d
tajho <alex.201894.lol@gmail.com>
2026-03-08 20:55:06 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,31 @@
-const CACHE_NAME = 'twins-cache-v12'; // Cambiamos a v12
-const urlsToCache = [
+const CACHE_NAME = 'twins-cache-v13';
+const ASSETS = [
   './',
-  './index.html?v=12',
-  './manifest.json?v=12',
-  './icon.png?v=11' // Forzamos la descarga del nuevo logo
+  './index.html',
+  './manifest.json',
+  './icon.png'
 ];
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
+    // Fuerza a la nueva versión a instalarse de inmediato
+    self.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    );
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-    ))
-  );
+    // Toma el control de la app inmediatamente sin esperar a cerrar sesión
+    event.waitUntil(clients.claim());
+    event.waitUntil(
+        caches.keys().then(keys => Promise.all(
+            keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+        ))
+    );
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(res => res || fetch(event.request))
-  );
+    event.respondWith(
+        caches.match(event.request).then(response => response || fetch(event.request))
+    );
 });
